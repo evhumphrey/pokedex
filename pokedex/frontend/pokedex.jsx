c6e8b54ca4748b19e971b5ee12aacf929951de0f
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-05-10 15:03:36 -0700

    Add apiutil to fetch pokemon, and action creator for receving all pokemon

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import * as APIUtil from './util/api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById("root");
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+
 });
