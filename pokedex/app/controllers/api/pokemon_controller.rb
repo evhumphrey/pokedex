48c1a5ecb63425209e8918dcf186342832dc9dcd
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-05-10 14:25:30 -0700

    Added jbuilder views for pokemon #index and #show

--- pokedex/app/controllers/api/pokemon_controller.rb
+++ pokedex/app/controllers/api/pokemon_controller.rb
@@ -1,8 +1,12 @@
 class Api::PokemonController < ApplicationController
   def index
+    @pokemon = Pokemon.all
+    render :index
   end
 
   def show
+    @pokemon = Pokemon.find_by(id: params[:id])
+    render :show
   end
 
   def create
